wuhao5/apr-util

commit 9c9b8fcb6e98f31cd459558379e75b31f618034d

Merge r1362241:
Avoid copying the hashed password to a temp buffer, if possible.

Noted by Jason Ovich <jasonovich mailfish de>
PR: 53410


git-svn-id: http://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x@1362243 13f79535-47bb-0310-9956-ffa450edef68

crypto/apr_passwd.c
@@ -98,7 +98,7 @@ APU_DECLARE(apr_status_t) apr_password_validate(const char *passwd,
          * It's not our algorithm, so feed it to crypt() if possible.
          */
 #if defined(WIN32) || defined(BEOS) || defined(NETWARE)
-        apr_cpystrn(sample, passwd, sizeof(sample) - 1);
+        return (strcmp(passwd, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 #elif defined(CRYPT_R_CRYPTD)
         CRYPTD buffer;
 
@@ -106,7 +106,7 @@ APU_DECLARE(apr_status_t) apr_password_validate(const char *passwd,
         if (!crypt_pw) {
             return APR_EMISMATCH;
         }
-        apr_cpystrn(sample, crypt_pw, sizeof(sample) - 1);
+        return (strcmp(crypt_pw, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 #elif defined(CRYPT_R_STRUCT_CRYPT_DATA)
         struct crypt_data buffer;
 
@@ -123,7 +123,7 @@ APU_DECLARE(apr_status_t) apr_password_validate(const char *passwd,
         if (!crypt_pw) {
             return APR_EMISMATCH;
         }
-        apr_cpystrn(sample, crypt_pw, sizeof(sample) - 1);
+        return (strcmp(crypt_pw, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 #else
         /* Do a bit of sanity checking since we know that crypt_r()
          * should always be used for threaded builds on AIX, and
@@ -143,8 +143,8 @@ APU_DECLARE(apr_status_t) apr_password_validate(const char *passwd,
             crypt_mutex_unlock();
             return APR_EMISMATCH;
         }
-        apr_cpystrn(sample, crypt_pw, sizeof(sample) - 1);
         crypt_mutex_unlock();
+        return (strcmp(crypt_pw, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 #endif
     }
     return (strcmp(sample, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;

commit 6e4847e064b75ed238d7d9d3d690e1b08d828892

merge r1370626 from trunk:
apr_password_validate(): Fix intermittent errors on systems
such as FreeBSD where the crypt() function is used.

(broken recently by r1362243)


git-svn-id: http://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x@1370627 13f79535-47bb-0310-9956-ffa450edef68

crypto/apr_passwd.c
@@ -134,18 +134,23 @@ APU_DECLARE(apr_status_t) apr_password_validate(const char *passwd,
 #if defined(_AIX) && APR_HAS_THREADS
 #error Configuration error!  crypt_r() should have been selected!
 #endif
+        {
+            apr_status_t rv;
 
-        /* Handle thread safety issues by holding a mutex around the
-         * call to crypt().
-         */
-        crypt_mutex_lock();
-        crypt_pw = crypt(passwd, hash);
-        if (!crypt_pw) {
+            /* Handle thread safety issues by holding a mutex around the
+             * call to crypt().
+             */
+            crypt_mutex_lock();
+            crypt_pw = crypt(passwd, hash);
+            if (!crypt_pw) {
+                rv = APR_EMISMATCH;
+            }
+            else {
+                rv = (strcmp(crypt_pw, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
+            }
             crypt_mutex_unlock();
-            return APR_EMISMATCH;
+            return rv;
         }
-        crypt_mutex_unlock();
-        return (strcmp(crypt_pw, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 #endif
     }
     return (strcmp(sample, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;